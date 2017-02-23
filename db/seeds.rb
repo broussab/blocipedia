d481090e21373e80597baf8727c636a6b8bf5768
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-02-23 17:05:17 -0600

    User Story 5 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require 'random_data'
 5.times do
   User.create!(
     # #3
-    username:     RandomData.random_username,
-    email:    RandomData.random_email,
-    password: RandomData.random_sentence
+    username: Faker::Internet.user_name,
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password(8)
   )
 end
 users = User.all
@@ -12,12 +12,18 @@ users = User.all
 20.times do
   wiki = Wiki.create!(
     user:   users.sample,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph,
+    title:  Faker::Hipster.sentence(2),
+    body:   Faker::Hipster.paragraph(3),
     private: false
   )
   wiki.update_attribute(:created_at, rand(10.minutes..1.year).ago)
 end
+
+User.create!(
+  username: 'Alyssa',
+  email:    'alybeic@gmail.com',
+  password: 'password'
+)
 
 puts 'Seed finished'
 puts "#{User.count} users created"
